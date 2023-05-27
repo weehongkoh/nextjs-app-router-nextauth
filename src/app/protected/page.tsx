f092eac53c04aa60453ec7eef9dc58bb41545061
WeeHong KOH <weehongayden.developer@hotmail.com>
2023-05-27 14:46:47 +0800

    revert changes

--- src/app/protected/page.tsx
+++ src/app/protected/page.tsx
@@ -3,7 +3,6 @@ import { getServerSession } from "next-auth";
 
 export default async function Protected() {
   const session = await getServerSession();
-  console.log(session);
   return (
     <main className="max-w-2xl min-h-screen flex flex-col items-center mx-auto">
       <div className="w-full flex justify-between my-10">
--- src/components/buttons/DiscordButton.tsx
+++ src/components/buttons/DiscordButton.tsx
@@ -10,7 +10,7 @@ export default function DiscordButton() {
     >
       <svg
         xmlns="http://www.w3.org/2000/svg"
-        enable-background="new 0 0 100 100"
+        enableBackground="new 0 0 100 100"
         viewBox="0 0 100 100"
         width="24"
         height="24"
